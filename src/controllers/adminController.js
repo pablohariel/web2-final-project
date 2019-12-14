989eaf322375c6036feacc48ea478c950f242099
pablohariel <pablohhdr@gmail.com>
2019-12-14 20:03:58 -0300

    comments removed

--- src/controllers/adminController.js
+++ src/controllers/adminController.js
@@ -5,72 +5,43 @@ exports.AddLots = async (req, res) => {
   
     var cont = 0;
     var result;
-    //console.log(result)
-    // while(cont < 10){
-        poster = false;
-    //   while(!poster) {
-        var random = Math.floor(Math.random() * (2199999 - 1000000) ) + 1000000; //2100367
-        //console.log("aleatorio: " + random);
-        await request("http://www.omdbapi.com/?i=tt"+ random +"&apikey=thewdb", function(error, response, body){
-            if(!error && response.statusCode == 200){
+    poster = false;
+    var random = Math.floor(Math.random() * (2199999 - 1000000) ) + 1000000; //2100367
+        
+    await request("http://www.omdbapi.com/?i=tt"+ random +"&apikey=thewdb", function(error, response, body){
+        
+        if(!error && response.statusCode == 200){
             result = JSON.parse(body);
             console.log(result);
             console.log(result["Poster"])
-                if(result!==undefined && result["Poster"] !== 'N/A'){
-                    const title = result["Title"];
-                    const date = result["Year"];
-                    const genre = result["Genre"];
-                    const description = result["Plot"];
-                    const img = result["Poster"];
-                    const movie = new Movie({
-                        title: title, 
-                        date: date, 
-                        genre: genre,
-                        description: description,
-                        img: img
-                    });
-                    movie
-                    .save()
-                    .then(result => {
-                        console.log('Filme criado!');
-                        res.redirect('/admin/addMovie');
-                    })
-                    .catch(error => {
-                        console.log(error);
-                    });
-                } else {
-                    res.redirect('/admin/addLots');
-                }
-            }
-      }
-      
-      ) }
-  //}
 
-/*  var movies = [];
-  var id = "tt005700";
-  //var i = "00";
-  
-  function fillArr(){
-      for(var i = 0; i<10; i++){
-          request("http://www.omdbapi.com/?i="+ id + i +"&apikey=thewdb", function(error, response, body){
-              if(!error && response.statusCode ==200){
-                  var results = JSON.parse(body);
-                  console.log("titulos no loop:" + results["Title"]);
-                  movies.push(results["Title"])
-              }
-          })
-      }
-      return movies;
-  }
-  fillArr()
-  .then(result => {
-      for(movie of movies){
-          console.log("titulo depois do loop: " + movie);
-      }
-  })
-  */
-// }
+            if(result!==undefined && result["Poster"] !== 'N/A'){
+                const title = result["Title"];
+                const date = result["Year"];
+                const genre = result["Genre"];
+                const description = result["Plot"];
+                const img = result["Poster"];
+                const movie = new Movie({
+                    title: title, 
+                    date: date, 
+                    genre: genre,
+                    description: description,
+                    img: img
+                });
+                movie.save()
+                .then(result => {
+                    console.log('Filme criado!');
+                    res.redirect('/admin/addMovie');
+                })
+                .catch(error => {
+                    console.log(error);
+                });
+            } else {
+                res.redirect('/admin/addLots');
+            }
+        }
+    });
+}
 
 exports.GetAddMovie = (req, res) => {
     if(req.session.isadmin){
